84448246fc7eb078197f2cb31a2f6ee2533d5d39
Jwah Uni <s3462188@student.rmit.edu.au>
2016-11-12 12:00:43 +1100

    Commands: Added git setup and puppet

--- packages.pp
+++ packages.pp
@@ -1,6 +1,23 @@
 # REF: https://docs.puppet.com/puppet/4.8/reference/types/package.html
+#
+
+
+# 3a
+package { 'openssh':
+        ensure => installed,
+        name => 'openssh',
+#        require => openssl
+}
+
+# 3b Apache
+
+
+
+
+#
 # EPEL needed for sshfs.
 # Need extras for epels though, should be enabled
+# 3 m
 package { 'epel':
         ensure => installed,
         name => 'epel-release',
